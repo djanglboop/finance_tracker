eb95badc3ab1217a823b51be91d26f756f0f2058
Yana Maximkina <djanglboop@gmail.com>
2019-11-30 13:23:18 +0900

    Add user and stock associations and views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     StockQuote::Stock.new(api_key: 'pk_9dd3e34faef940f39f43eec9dd325b71')
     begin
